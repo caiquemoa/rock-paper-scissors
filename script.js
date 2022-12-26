b34fac0bd2c2397984982e7f670b3b9fa7982961
caiquemoa <caiquemoa@gmail.com>
2022-12-26 11:48:55 -0300

    Implement playRound() function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function getComputerChoice() {
 	const options = ['Rock', 'Paper', 'Scissors']
 	return options[Math.floor(Math.random() * 3)]
 }
+
+function playRound(p, computer) {
+	const player = p.charAt(0).toUpperCase() + p.slice(1).toLowerCase()
+
+	if (player === computer) return 'Its a draw'
+	else if (
+		(player === 'Rock' && computer === 'Scissors') ||
+		(player === 'Paper' && computer === 'Rock') ||
+		(player === 'Scissors' && computer === 'Paper')
+	)
+		return `You Won ${player} beats ${computer}`
+	else return `You Lose! ${computer} beats ${player} `
+}
