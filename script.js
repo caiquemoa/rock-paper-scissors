faf4d1da8bc557189742346292a1cf20851ccddf
caiquemoa <caiquemoa@gmail.com>
2022-12-27 18:01:39 -0300

    Game logic

--- script.js
+++ script.js
@@ -12,6 +12,21 @@ function playRound(p, computer) {
 		(player === 'Paper' && computer === 'Rock') ||
 		(player === 'Scissors' && computer === 'Paper')
 	)
-		return `You Won ${player} beats ${computer}`
+		return `You Won! ${player} beats ${computer}`
 	else return `You Lose! ${computer} beats ${player} `
 }
+
+function game() {
+	let computerScore = 0
+	let playerScore = 0
+	for (let i = 0; i < 5; i++) {
+		const computerChoice = getComputerChoice()
+		const playerChoice = prompt('Your Choice')
+		const round = playRound(playerChoice, computerChoice)
+		console.log(round)
+		if (round.includes('Won')) playerScore++
+		else if (round.includes('Lose')) computerScore++
+		console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
+	}
+}
+game()
